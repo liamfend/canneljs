ab3819babcee04cb8ea900e6727fdd8d24f8ac6d
Liam.Li <398389303@qq.com>
2021-07-09 01:26:11 +0800

    ok

--- packages/cps-stories/src/MyButton.stories.tsx
+++ packages/cps-stories/src/MyButton.stories.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Button } from './Button';
+import 'cps-theme/src/button.scss';
+import { Button } from 'cps';
 
 export default {
   title: 'Mobile/MyButton',
--- packages/cps/components/button/Button.tsx
+++ packages/cps/components/button/Button.tsx
@@ -1,10 +1,10 @@
 import React, { DOMAttributes, ReactElement } from 'react';
 
-interface IAlert extends DOMAttributes<HTMLButtonElement> {
+interface IButton extends DOMAttributes<HTMLButtonElement> {
   content: string;
 }
 
-export default function Alert({ content, ...rest }: IAlert): ReactElement {
+export default function Button({ content, ...rest }: IButton): ReactElement {
   return (
     <div>
       <button {...rest}>{content}</button>
