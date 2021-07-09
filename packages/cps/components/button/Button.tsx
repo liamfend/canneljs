e15288f0ef308af69e425fe7a2e76fc26fa21d1d
Liam Li <398389303@qq.com>
2021-07-09 23:28:23 +0800

    comp

--- packages/cps/components/button/Button.tsx
+++ packages/cps/components/button/Button.tsx
@@ -4,14 +4,10 @@ interface IButton extends DOMAttributes<HTMLButtonElement> {
   content: string;
 }
 
-export default function Button({
-  content,
-
-  ...rest
-}: IButton): ReactElement {
+export default function Button({ content, ...rest }: IButton): ReactElement {
   return (
     <button type="button" className={'storybook-button'} {...rest}>
-      {content}
+      {content} 1321321313 1111dsafdsa
     </button>
   );
 }
